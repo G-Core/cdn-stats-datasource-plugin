d1387c13350e600e0e44eb667c56e13e68b6f245
oborodin <oleg.borodin@gcore.com>
2025-09-09 21:16:23 +0200

    Migration to Grafana 12

--- src/components/GCSelectGranularity.tsx
+++ src/components/GCSelectGranularity.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { SelectCommonProps } from "@grafana/ui/components/Select/types";
 import { GCGranularity } from "../types";
-import { Select } from "@grafana/ui";
+import { Select, SelectCommonProps } from "@grafana/ui";
 import { createOptions } from "granularity";
 
 export const GCSelectGranularity: React.FC<
